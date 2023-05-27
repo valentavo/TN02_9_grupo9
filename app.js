123db885f23ae431499a9d6e41b0706b8b1f2c1f
Ramiro Saponaro <r.saponaro@hotmail.com>
2023-05-27 20:28:01 -0300

    commit registro

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/index.html"))
 } )
 
-app.get("/login", (req, res) =>{
+app.get("/  ", (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/login.html"))
 } )
 
