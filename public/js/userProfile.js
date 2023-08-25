6aa416afa19f3610b5868170ba0b1ea0f6866a78
mflorschmidt <mariaf.schmidt33@gmail.com>
2023-08-25 13:55:46 +0800

    arreglo de vista de perfil de usuario

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -28,8 +28,10 @@ async function ready() {
     userBirth.innerHTML += `<input type="date" class="form-control" id="user-birth" value="${ user['fecha-nacimiento'] || "" }" name="birth">`
 
     if(user['roles-fk'] == 2) {
-        productList.innerHTML += `<button class="nav-link" type="button">
-                                    <a href="/product/list">Lista de productos</a>
+        productList.innerHTML += `<button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+                                <i class="text-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box2-heart-fill" viewBox="0 0 16 16">
+                                <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4h-8.5ZM8.5 4h6l.5.667V5H1v-.333L1.5 4h6V1h1v3ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                              </svg></i><a href="/product/list"> Lista de productos</a>
                                 </button>`;
     };
 
